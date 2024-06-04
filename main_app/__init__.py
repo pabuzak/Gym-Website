1985cf4a5ffa1073ee41d1162c4a4257e5e6291d
pabuzak <pabuzak@gmail.com>
2024-06-05 00:02:28 +1200

    Hiding Keys in Environment Variables

--- main_app/__init__.py
+++ main_app/__init__.py
@@ -4,10 +4,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+import os
 
 app = Flask(__name__)
 # Website secret key
-app.config['SECRET_KEY'] = '07103e0040d8c8c30338bfd0072dcc54'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 # Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
@@ -17,11 +18,11 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 # Mail server
-app.config['MAIL_SERVER'] = 'mail.glasse.co.nz'
+app.config['MAIL_SERVER'] = os.environ.get('DB_MAILSERVER')
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USERNAME'] = 'reset@glasse.co.nz'
-app.config['MAIL_PASSWORD'] = 'SanctaDTG22!@'
+app.config['MAIL_USERNAME'] = os.environ.get('DB_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('DB_PASSWORD')
 mail = Mail(app)
 
 from main_app import routes
